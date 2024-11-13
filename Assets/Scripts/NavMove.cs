2ea8583ca3dde8450a2b028b3f860fa3b79ba083
Arden Feldt <feldt@ad.unc.edu>
2024-11-13 14:29:31 -0500

    successful first navmesh run

--- Assets/Scripts/NavMove.cs
+++ Assets/Scripts/NavMove.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class NavMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public UnityEngine.AI.NavMeshAgent agent;
+    public GameObject waypoint;
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(waypoint.transform.position);
     }
 }
